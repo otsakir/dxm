e7675dac4d00b89202c4143d4d346f003e442b2d
Orestis Tsakiridis <otsakir@gmail.com>
2015-10-14 01:11:48 +0300

    Added angular-route lib and basic view routing. Also enabled a map o the home view.

--- app/public/scripts/app.js
+++ app/public/scripts/app.js
@@ -1,1 +1,24 @@
-angular.module('dxm', ['ui.bootstrap']);
+angular.module('dxm', [
+	'ngRoute',
+	'ui.bootstrap',
+	'uiGmapgoogle-maps'
+]);
+
+angular.module('dxm').config(function($routeProvider, $locationProvider) {
+	$routeProvider
+	.when('/', {
+		templateUrl: 'templates/home.html',
+		controller: 'homeCtrl'
+	})
+	.otherwise({
+		redirectTo: "/"
+	});
+});
+
+angular.module('dxm').config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        //    key: 'your api key',
+        v: '3.20', //defaults to latest 3.X anyhow
+        libraries: 'weather,geometry,visualization'
+    });
+})
--- app/public/scripts/controllers.js
+++ app/public/scripts/controllers.js
@@ -1,5 +1,8 @@
-
-
 angular.module("dxm").controller("MainCtrl",function ($scope) {
 	$scope.hello = "Hello World!";
 });
+
+angular.module("dxm").controller("homeCtrl",function ($scope) {
+	$scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+});
+
