b86d39ec2fc9b4cedc88f4b841e5b1744f5b1486
Orestis Tsakiridis <otsakir@gmail.com>
2015-10-14 00:27:32 +0300

    Added get_user() method. Found out how to capture path parameters in nginx variables

--- app/lua/users.lua
+++ app/lua/users.lua
@@ -1,7 +1,6 @@
 local pgmoon = require "pgmoon"
 local cjson = require "cjson"
 
-
 local pg = pgmoon.new({
   host = "127.0.0.1",
   port = "5432",
@@ -15,10 +14,29 @@ function list_users()
 	return res
 end
 
+function get_user(userid)
+	local res = assert(pg:query("select * from users where username = '"..userid.."'"))
+	if #res > 0 then
+		return 200, res[1]
+	else
+		return 404
+	end
+end
+
 assert(pg:connect())
 
 if (ngx.var.request_method == "GET") then
-	ngx.say(cjson.encode(list_users()))
+	if ngx.var.userid then
+		local status, user = get_user(ngx.var.userid)
+		if status == 200 then 
+			ngx.say(cjson.encode(user))
+		else
+			--ngx.status = ngx.HTTP_NOT_FOUND
+			ngx.exit(ngx.HTTP_NOT_FOUND)
+		end
+	else
+		ngx.say(cjson.encode(list_users()))
+	end
 elseif (ngx.var.request_method == "POST") then
 	ngx.say("received POST")
 end
