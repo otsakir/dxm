f0d32660e27e3a3baa4fbf6b264213a5f86d24be
Orestis Tsakiridis <otsakir@gmail.com>
2015-10-20 00:33:35 +0300

    Experimenting with marker and window classes [UI].

--- app/public/scripts/controllers.js
+++ app/public/scripts/controllers.js
@@ -8,8 +8,13 @@ angular.module("dxm").controller("homeCtrl",function ($scope) {
 
 angular.module("dxm").controller("browseCtrl",function ($scope, $resource) {
 	var Locations = $resource("api/locations");
+	$scope.someHtml = "<div class='marker-label'>bla bla</div>";
 	$scope.locations = Locations.query();
 	// 37.9908996, 23.7032341 - athens
-	$scope.map = { center: { latitude: 37.9908996, longitude: 23.7032341 }, zoom: 4 };
+	$scope.map = { center: { latitude: 37.9908996, longitude: 23.7032341 }, zoom: 8 };
+	
+	$scope.markerClicked = function (location) {
+		console.log("clicked on marker " + location.name);
+	}
 });
 
