e6b6496326e709799fc33f59e2a19259169e4fba
Orestis Tsakiridis <otsakir@gmail.com>
2015-11-16 22:18:08 +0200

    Used new 'dispatcher' functionality in users endpoint

--- app/lua/goods.lua
+++ app/lua/goods.lua
@@ -29,26 +29,3 @@ dispatcher.dispatch("^/api/goods")
 
 return
 
-
-
-
---[[
-local pg = storage.connect_db()
-assert(pg:connect())
-
-if (ngx.var.request_method == "POST") then
-	ngx.log(ngx.ERR,"POST /api/goods not implemented yet!")
-	ngx.exit(404)
-elseif (ngx.var.request_method == "GET" and not ngx.var.goodid) then
-	local res, message = storage.list_goods(pg)
-	if not res then
-		ngx.exit(404) -- treat all errors as NOT_FOUND
-	else
-		ngx.say(cjson.encode(res))
-	end
-elseif (ngx.var.request_method == "GET" and ngx.var.goodid) then
-	ngx.log(ngx.ERR,"POST /api/goods/{goodid} not implemented yet!")
-	ngx.exit(404)
-end
-
-]]--
--- app/lua/users.lua
+++ app/lua/users.lua
@@ -1,23 +1,33 @@
 local cjson = require "cjson"
 local storage = require "storage"
+local dispatcher = require "dispatcher"
 
 local pg = storage.connect_db()
 assert(pg:connect())
 
-if (ngx.var.request_method == "GET") then
-	if ngx.var.userid then
-		local status, user = storage.get_user(pg,ngx.var.userid)
-		if status == 200 then 
-			ngx.say(cjson.encode(user))
-		else
-			--ngx.status = ngx.HTTP_NOT_FOUND
-			ngx.exit(ngx.HTTP_NOT_FOUND)
-		end
+function get_single_user(matches)
+	local userid = matches[1]
+	ngx.log(ngx.ERR, "userid: "..userid)
+	local status, user = storage.get_user(pg,userid)
+	if status == 200 then 
+		ngx.say(cjson.encode(user))
 	else
-		ngx.say(cjson.encode(storage.list_users(pg)))
+		ngx.exit(ngx.HTTP_NOT_FOUND)
 	end
-elseif (ngx.var.request_method == "POST") then
-	ngx.say("received POST")
+end
+
+function get_users()
+	ngx.say(cjson.encode(storage.list_users(pg)))
+end
+
+function add_user()
+	ngx.exit(501)
 end
 
 
+
+
+dispatcher.addrule("GET","/(.+)$",get_single_user)
+dispatcher.addrule("GET","/?$",get_users)
+dispatcher.addrule("POST","/$",add_user)
+dispatcher.dispatch("^/api/users")
